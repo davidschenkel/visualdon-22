2fca1ffdcea4049640e24a6f8047b65d847bcfb2
David Schenkel <david.schenkel@heig-vd.ch>
2022-03-03 17:24:15 +0100

    exo 1 done

---
+++ 01-SVG/js/script.js
@@ -0,0 +1,44 @@
+// Quelques fonctions utilitaires
+function domForEach(selector, callback) {
+  document.querySelectorAll(selector).forEach(callback);
+}
+function domOn(selector, event, callback, options) {
+  document.querySelectorAll(selector).forEach(element => element.addEventListener(event, callback, options));
+}
+
+
+
+// changer la couleur du rectangle quand on clique dessus
+domOn('#changeColor', 'click', evt => {
+
+let maxVal = 0xFFFFFF; // 16777215
+let randomNumber = Math.random() * maxVal; 
+randomNumber = Math.floor(randomNumber);
+let randColor = randomNumber.toString(16);
+
+  const el = evt.currentTarget;
+  if (el.style.fill === 'red') {
+    el.style.fill = `#${randColor}`;
+  }
+  else el.style.fill = 'red';
+
+  const element = document.getElementById('color');
+  
+  if (element.style.fill === 'red') {
+    element.style.fill = `#${randColor}`;
+  }
+  else element.style.fill = 'red';
+});
+
+
+
+
+// changer la taille du cercle extérieur au passage de la souris
+domOn('#changeSize', 'mouseover', event => {
+  event.currentTarget.style.r ='80';
+  
+
+})
+
+
+
