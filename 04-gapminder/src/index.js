94711578bc0104d7481f5a4efae31a462893a56f
David Schenkel <david.schenkel@heig-vd.ch>
2022-04-28 14:41:23 +0200

    exo 4 part 2 done

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -74,7 +74,7 @@ let svg1 = d3.select("#graph")
 
 
 let x = d3.scaleLinear()
-    .domain([0, 100000])
+    .domain([0, 130000])
     .range([0, width]);
 
 const y = d3.scaleLinear()
@@ -106,7 +106,100 @@ groupe.selectAll("circle")
     // .style("opacity", "0.7")
     .attr("stroke", "blue")
 
+// Cartographie 
 
+const margin2 = { top: 10, right: 40, bottom: 30, left: 30 };
+const width2 = 800 - margin.left - margin.right;
+const height2 = 500 - margin.top - margin.bottom;
 
+d3.select("body")
+    .append("div")
+    .attr('id', 'carte')
+let svg2 = d3.select("#carte")
+    .append("svg")
+    .attr("width", width + margin2.left + margin2.right)
+    .attr("height", height + margin2.top + margin2.bottom)
+    .append("g").attr("class", "group2")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")")
 
+const groupe2 = d3.select(".group2");
 
+// Map and projection
+const path = d3.geoPath();
+const projection = d3.geoMercator()
+    .scale(85)
+    .center([0, 45])
+    .translate([width / 2, height / 2]);
+
+let colorScale = d3.scaleThreshold()
+    .domain([60, 79, 80])
+    .range(['green', 'lightblue', 'orange']);
+
+// Load external data and boot
+Promise.all([
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+]).then(function (loadData) {
+
+        
+        
+
+    let topo = loadData[0]
+    console.log(topo);
+    
+    
+    
+
+    let mouseOver = function (d) {
+        d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", .5)
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("opacity", 1)
+            .style("stroke", "red")
+    }
+
+    let mouseLeave = function (d) {
+        d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", .8)
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("stroke", "transparent")
+    }
+
+    // Draw the map
+    groupe2.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d, i) {
+            const country = y2021.filter(y2021 => y2021.country == d.properties.name)
+            let lifeExp = ''
+            console.log(country[0]);
+            
+            if (country[0] === undefined) {
+                lifeExp = 0;
+            } else {
+                lifeExp = country[0].life
+            }
+            return colorScale(lifeExp);
+        })
+        .style("stroke", "white")
+        .attr("class", function (d) {
+            return d.properties.name
+        })
+        .style("opacity", .8)
+        .on("mouseover", mouseOver)
+        .on("mouseleave", mouseLeave)
+})
